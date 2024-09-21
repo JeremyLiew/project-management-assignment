3e82d5b5436cf2799061d4508d8e19e0de5611b3
JeremyLiew <jeremyliew@etctech.com.my>
2024-09-22 01:45:25 +0800

    default value for system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Budget;
-use App\Models\Expense;
-use App\Models\ExpenseCategory;
-use App\Models\Project;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,11 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class); 
+        $this->call(UserSeeder::class);
 
-        Budget::factory()->count(1)->create();
-        ExpenseCategory::factory()->count(2)->create();
-        Project::factory()->create();
-        Expense::factory(5)->create();
     }
 }
